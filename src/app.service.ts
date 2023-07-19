6cec54467cfcd9e92f4e2f5f6cb34b9c2da79363
SBraunovicOmega <stefan.braunovic@vegait.rs>
2023-07-19 12:18:48 +0200

    DB connection to the AWS & Model created

---
+++ src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {}
+
+
+
+// 1) Install Postrgres on local machine localhost:5432
+
+// 2) Host Postgres in cloud
